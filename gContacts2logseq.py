28c995570754eff05846cf60aa7b552f2254371a
Spiros Vathis <spiros.vathis@gmail.com>
2024-12-01 20:06:03 +0200

    Add token refreshing mechanism to ensure long-term access
    - Implemented a `login` function to handle OAuth 2.0 token refreshing.
    - Modified the code to use the `login` function for obtaining credentials.
    - Ensured that the access token is refreshed using the refresh token when expired.
    - Updated the `service` initialization to use the refreshed credentials.

--- gContacts2logseq.py
+++ gContacts2logseq.py
@@ -154,31 +154,29 @@ class md_person:
 
 
 # https://developers.google.com/people/quickstart/python
+TOKEN_FILE = 'token.json'
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/contacts.readonly','https://www.googleapis.com/auth/user.addresses.read']
 
 
 def login():
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
     creds = None
-
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
+    # Load credentials from the token file if it exists
+    if os.path.exists(TOKEN_FILE):
+        with open(TOKEN_FILE, 'r') as token:
+            creds = Credentials.from_authorized_user_info(json.load(token), SCOPES)
+    # If there are no valid credentials available, let the user log in
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
                 'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=8081, open_browser=False)
+            creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open('token.json', 'w') as token:
+        with open(TOKEN_FILE, 'w') as token:
             token.write(creds.to_json())
-
     return creds
 
 
